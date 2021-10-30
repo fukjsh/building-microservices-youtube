c8570c50f546a7e53f5410d905f6d212919de542
Josh Cowling <cowlingjoshua@gmail.com>
2021-10-30 15:26:46 +0100

    clean up address

--- product-api/main.go
+++ product-api/main.go
@@ -9,14 +9,9 @@ import (
 	"time"
 
 	"github.com/nicholasjackson/building-microservices-youtube/product-api/handlers"
-	"github.com/nicholasjackson/env"
 )
 
-var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
-
 func main() {
-
-	env.Parse()
 
 	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
 
